db1a75ae4dc65adc861f9ab899f9f0dcf887754b
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-08-02 08:34:25 -0300

    Criando as Rotas E o Componente

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosRoutingModule } from './cursos-routing.module';
+import { ListacursosComponent } from './listacursos/listacursos.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListacursosComponent
+  ],
   imports: [
     CommonModule,
     CursosRoutingModule
---
+++ src/app/cursos/listacursos/listacursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listacursos',
+  templateUrl: './listacursos.component.html',
+  styleUrls: ['./listacursos.component.scss']
+})
+export class ListacursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
