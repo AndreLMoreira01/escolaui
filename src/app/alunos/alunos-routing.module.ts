8abe4fcc2b3d46a82ddd2b87f55bc0d36335a3e8
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-08-09 08:44:48 -0300

    Ligando as rotas

--- src/app/alunos/alunos-routing.module.ts
+++ src/app/alunos/alunos-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlunosComponent } from './alunos/alunos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '', component: AlunosComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
