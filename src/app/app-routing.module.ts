1ccb08f15b5f07f26a825230e0c795005bf1116e
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-08-09 08:26:32 -0300

    Rotas para o curso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+path: 'cursos',
+loadChildren: ()=> import('./cursos/cursos.module').then(m => m.CursosModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/cursos/cursos-routing.module.ts
+++ src/app/cursos/cursos-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListacursosComponent } from './listacursos/listacursos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '', component: ListacursosComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
