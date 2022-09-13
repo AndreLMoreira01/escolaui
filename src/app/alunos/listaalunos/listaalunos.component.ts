97b17f0066b04fb8f6e10414c88d92197150d568
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-09-13 08:44:42 -0300

    Fazendo o Mapa de Alunos

--- src/app/alunos/listaalunos/listaalunos.component.ts
+++ src/app/alunos/listaalunos/listaalunos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Aluno } from 'src/app/core/model';
 
 @Component({
   selector: 'app-listaalunos',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listaalunos.component.scss']
 })
 export class ListaalunosComponent implements OnInit {
+
+  alunod: Aluno[] = [
+
+    {"id":1, "nomealuno": 'Desenvolvimento de Sistemas'}
+
+  ];
+  displayedColumns = ['id','nomealuno'];
 
   constructor() { }
 
